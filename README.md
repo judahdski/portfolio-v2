edb72f19e12e1d1d34de493978de3b6353ed8f7d
imJudah <judahdasuki93@gmail.com>
2026-08-26 10:47:51 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+ini buat:
+
+"pnpm exec prettier . --write" =>
+
+"pnpm exec prettier . --check" =>
+
+"pnpm lint" =>
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ArrowRight } from 'lucide-react'
+
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Portfolio</h1>
+
+      <div className="font-inter">inter font</div>
+      <div className="font-montserrat">montserrat font</div>
+
+      <ArrowRight size={32} />
+    </main>
+  )
+}
+
+export default App
